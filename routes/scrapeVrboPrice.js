f3ff61696ee54d8f84f36c2dde02ccc1509e3517
Anton Khlyzov <akhlyzov@myseneca.ca>
2024-06-23 22:24:04 -0400

    10th  commit

--- routes/scrapeVrboPrice.js
+++ routes/scrapeVrboPrice.js
@@ -5,31 +5,23 @@ puppeteer.use(StealthPlugin());
 async function scrapeVrboPrice(vrboUrl) {
     const browser = await puppeteer.launch({
         headless: 'new',
-        executablePath: process.env.CHROME_EXECUTABLE_PATH || '/opt/render/.cache/puppeteer/chrome-linux/chrome',
+        executablePath: puppeteer.executablePath(),
         args: ['--no-sandbox', '--disable-setuid-sandbox']
     });
-    
+
     const page = await browser.newPage();
-
     try {
-        // Navigate to the VRBO page
         await page.goto(vrboUrl);
-
-        // Wait for the price element to load
         await page.waitForSelector('#pdp-search-form span > div');
-
-        // Extract the price text from the element
         const price = await page.$eval('#pdp-search-form span > div', element => element.textContent.trim());
-
         return price;
-
     } catch (error) {
         console.error('Error during VRBO scraping:', error.message);
         return '$250'; // Handle the error gracefully
     } finally {
-        // Close the browser
         await browser.close();
     }
 }
 
 module.exports = { scrapeVrboPrice };
+
