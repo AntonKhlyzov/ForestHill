80490f6c2dc3ea43f1d1cfd84855da5081bcb084
Anton Khlyzov <akhlyzov@myseneca.ca>
2024-06-29 21:33:33 -0400

    docker build 2

--- routes/scrapeVrboPrice.js
+++ routes/scrapeVrboPrice.js
@@ -2,22 +2,29 @@ const puppeteer = require('puppeteer-extra');
 require("dotenv").config();
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
 puppeteer.use(StealthPlugin());
-console.log(puppeteer.executablePath());
+
+console.log('Puppeteer Executable Path:', puppeteer.executablePath());
 
 async function scrapeVrboPrice(vrboUrl) {
     const browser = await puppeteer.launch({
-       args: ['--no-sandbox', '--disable-setuid-sandbox', '--single-process', '--no-zygote',],
+       args: ['--no-sandbox', '--disable-setuid-sandbox', '--single-process', '--no-zygote'],
        executablePath: 
         process.env.NODE_ENV === 'production' 
         ? process.env.PUPPETEER_EXECUTABLE_PATH
-            : puppeteer.executablePath(),
+        : puppeteer.executablePath(),
     });
 
     const page = await browser.newPage();
     try {
-        await page.goto(vrboUrl);
-        await page.waitForSelector('#pdp-search-form span > div');
+        console.log('Navigating to VRBO URL:', vrboUrl);
+        await page.goto(vrboUrl, { waitUntil: 'networkidle2' });
+
+        console.log('Waiting for price selector to appear...');
+        await page.waitForSelector('#pdp-search-form span > div', { timeout: 10000 }); // 10 seconds timeout
+
+        console.log('Selector found, extracting price...');
         const price = await page.$eval('#pdp-search-form span > div', element => element.textContent.trim());
+        console.log('Extracted Price:', price);
         return price;
     } catch (error) {
         console.error('Error during VRBO scraping:', error.message);
@@ -28,4 +35,3 @@ async function scrapeVrboPrice(vrboUrl) {
 }
 
 module.exports = { scrapeVrboPrice };
-
