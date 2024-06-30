5f2a2e37baab48d2c057c3cdd26aeb83993ddc03
Anton Khlyzov <akhlyzov@myseneca.ca>
2024-06-29 22:59:44 -0400

    scraping logic changes

--- routes/scrapeVrboPrice.js
+++ routes/scrapeVrboPrice.js
@@ -1,28 +1,35 @@
 const puppeteer = require('puppeteer-extra');
-require("dotenv").config();
 const StealthPlugin = require('puppeteer-extra-plugin-stealth');
+require('dotenv').config();
+
 puppeteer.use(StealthPlugin());
-console.log(puppeteer.executablePath());
 
 async function scrapeVrboPrice(vrboUrl) {
     const browser = await puppeteer.launch({
-       args: [`--no-sandbox`, `--headless`, `--disable-gpu`, `--disable-dev-shm-usage`,`--disable-setuid-sandbox`, `--single-process`, `--no-zygote`],
-       headless: true,
-       executablePath: 
-        process.env.NODE_ENV === 'production' 
-        ? process.env.PUPPETEER_EXECUTABLE_PATH
+        headless: true,
+        executablePath: process.env.NODE_ENV === 'production'
+            ? process.env.PUPPETEER_EXECUTABLE_PATH
             : puppeteer.executablePath(),
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-gpu',
+            '--headless',
+            '--single-process',
+            '--no-zygote'
+        ],
     });
 
     const page = await browser.newPage();
     try {
-        await page.goto(vrboUrl);
-        await page.waitForSelector('#pdp-search-form span > div');
+        await page.goto(vrboUrl, { waitUntil: 'networkidle2' }); // Wait for network idle
+        await page.waitForSelector('#pdp-search-form span > div', { timeout: 30000 }); // Wait for price selector
         const price = await page.$eval('#pdp-search-form span > div', element => element.textContent.trim());
         return price;
     } catch (error) {
         console.error('Error during VRBO scraping:', error.message);
-        return '$250'; // Handle the error gracefully
+        return '$250'; // Handle the error gracefully with a default price
     } finally {
         await browser.close();
     }
